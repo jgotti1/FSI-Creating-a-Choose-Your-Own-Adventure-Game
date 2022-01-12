456775daeeeb7dabc3d3516a09243a78149592db
jgotti1 <jgotti1@optonline.net>
2022-01-12 14:29:03 -0500

    first version

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your Code Here
+let answer  = window.prompt('Do you head left or right?')
+    if(answer==='left') {
+     window.alert('Left was a great choice click OK to see more...');
+     window.location.replace("left.html");
+}
+    else if(answer==='right') {
+    window.alert('May not have been the best choice, click ok to see more....')
+    window.location.replace("right.html");
+}
+    else {
+    window.alert('That is not a choice you dummy!');
+    window.location.replace("huggy.html");
+    
+}
